304a84868f674516d72a742fb57a2994db762f62
fred_chen <ltscyt0717@gmail.com>
2022-09-03 17:01:43 +0800

    commit luna

--- luna.py
+++ luna.py
@@ -2,38 +2,41 @@ import pyautogui as p
 import pydirectinput as pp
 import time
 
-p.PAUSE = 1
-pp.PAUSE = 1
+p.PAUSE = 0.1  
+pp.PAUSE = 0.1
 
 logo_pos=p.locateCenterOnScreen('./luna_image/logo.png')
 
-while True:
-    if logo_pos == None:
-        print('Not found luna, finish')
-        break
-    else:
-        p.moveTo(logo_pos)
-        p.click()
 
+p.click(100, 10)
+
+while True:
     buff_dict = dict({
         'exp_2h_10pa': '6',
-        'mp': '7',
-        'exp_2h_20pa':'8',
-        'exp_1h_100pa': '9',
+        'mp': '9',
+        'exp_2h_20pa': '7',
+        'exp_1h_50pa': '8',
     })
 
     for buff, press_key in buff_dict.items():
         print(buff, press_key)
         buff_pos = p.locateCenterOnScreen('./luna_image/{buff}.png'.format(buff = buff))
-        while True: 
-            if buff_pos == None:
-                print('Not found\nUse {buff}'.format(buff=buff))
-                pp.press(press_key)
-                pp.press('enter')
-                pp.press('enter')
-            else:
-                print(buff_pos)
-                print('still have {buff}'.format(buff=buff))
-                buff_pos = None
-                break
-    time.sleep(10)
+        if buff_pos == None:
+            print('Not found\nUse {buff}'.format(buff=buff))
+            pp.press(press_key)
+            pp.press('enter')
+            pp.press('enter')
+        else:
+            print(buff_pos)
+            print('still have {buff}'.format(buff=buff))
+            buff_pos = None
+    
+    while True:
+        pos=p.locateOnScreen('./luna_image/tab_monster.png')
+        if pos == None:
+            pp.press('tab')
+        else:
+            print('found!')
+            break
+    pp.press('1')
+    time.sleep(3)
